43b0f347a85dcc0508429f16fdf656f970685aaa
Srini Kusunam <skusunam@gmail.com>
2014-08-27 21:40:54 +0530

    Added ng-grid advanced options

--- src/js/homeController.js
+++ src/js/homeController.js
@@ -10,7 +10,13 @@ angular.module('angularGruntSeed')
         });
 
         $scope.gridOptions = {
-            data: 'myData'
+            data: 'myData',
+            // Column definition example
+            columnDefs: [{field:'id', cellTemplate: '<div ng-class="{green: row.getProperty(col.field) > 1}"><div class="ngCellText">{{row.getProperty(col.field)}}</div></div>'},{field:'firstname', displayName:'First Name'}, {field:'lastname', displayName:'Last Name'},{field:'company'},{field:'project'}],
+            rowTemplate:'<div style="height: 100%" ng-class="{green: row.getProperty(\'id\') > 2 }"><div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell ">' +
+                           '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }"> </div>' +
+                           '<div ng-cell></div>' +
+                     '</div></div>'
         };
     }
 ]);
