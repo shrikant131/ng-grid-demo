8d0598565a6ca6498d579a37087ab07ab1e247c7
Srini Kusunam <skusunam@gmail.com>
2014-08-27 11:59:01 +0530

    Initial commit with seed project

---
+++ src/js/homeController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('angularGruntSeed')
+
+.controller('HomeController', ['$scope',
+    function($scope, gridService) {
+        console.log('HomeController called .................');
+    }
+]);
